f92da75b722c84cc70bb8541ec4fbdd3a43c9c36
kawakami-masachika <supercell1023koeda@gmail.com>
2025-06-08 19:14:21 +0900

    refactor

--- main.rb
+++ main.rb
@@ -1,13 +1,15 @@
 require_relative './src/zenn_base_service'
 require_relative './src/notion_base_service'
+
 # 取得したいZennのトピックを指定
-TARGET_TOPIC = "ai" # ここを好きなトピック名に変更してください
+DEFAULT_TOPIC = "ai" # ここを好きなトピック名に変更してください
 
 def main
   # トピックを標準入力で受け取る
   puts "Enter a topic to fetch articles from Zenn (default: 'ruby'):"
   topic = gets.chomp
-  topic = "ruby" if topic.strip.empty? # 入力が空の場合はデフォルト値を使用
+  # 入力が空の場合はデフォルト値を使用
+  topic = DEFAULT_TOPIC if topic.strip.empty?
 
   # Notionへのアクションを標準入力で受け取る
   puts "Enter an action for Notion (add or skip, default: 'add'):"
@@ -15,7 +17,10 @@ def main
   action = "add" if action.strip.empty? # 入力が空の場合はデフォルト値を使用
 
   # Zennの記事を取得
-  articles = ZennBaseService.new.get_zenn_articles(topic)
+  articles = ZennBaseService.new({
+    topic: topic,
+  }).get_zenn_articles
+
   notion = NotionBaseService.new
 
   # アクションに応じて処理を実行
@@ -27,8 +32,6 @@ def main
     puts "Skipping Notion action."
   end
 end
-
-# TODO: 重複チェック処理をここに実装することを推奨します
 
 # mainメソッドを呼び出す
 main
--- src/zenn_base_service.rb
+++ src/zenn_base_service.rb
@@ -1,18 +1,22 @@
 require 'httparty'
 
 class ZennBaseService
-  ZENN_API_ENDPOINT = "https://zenn.dev/api/articles"
+  ZENN_API_ENDPOINT = "https://zenn.dev/api/articles".freeze # Zenn APIのエンドポイント
+  DEFAULT_COUNT = 10.freeze # デフォルトで取得する記事数
 
-  def initialize; end
+  def initialize(options)
+    @count = options[:count] || DEFAULT_COUNT
+    @topic = options[:topic]
+  end
 
   # Zenn APIから記事を取得するメソッド
-  def get_zenn_articles(topic)
-    puts "Fetching articles for topic: #{topic}..."
+  def get_zenn_articles
+    puts "「#{topic}」トピックの記事を取得中... (最大 #{@count} 件)"
     options = {
       query: {
         topicname: topic,
         order: "latest",
-        count: 10
+        count: @count
       }
     }
     response = HTTParty.get(ZENN_API_ENDPOINT, options)
@@ -26,4 +30,7 @@ class ZennBaseService
     puts "Successfully fetched #{articles.length} articles."
     articles
   end
+
+  private
+  attr_reader :count, :topic
 end
